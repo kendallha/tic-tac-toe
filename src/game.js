7b943c0bf29e16f5d150ddd933da0445f7aeb139
kendallha <kendallhaw@gmail.com>
2021-03-03 17:34:08 -0800

    Update game play functions and add startGame function to main js file

--- src/game.js
+++ src/game.js
@@ -1,55 +1,54 @@
 class Game {
   constructor(id) {
-    this.player1 = new Player(1, token);
-    this.player2 = new Player(2, token);
-    this.turn = player1;
+    this.player1 = new Player(1, 'frog');
+    this.player2 = new Player(2, 'sheep');
+    this.turn = 'player1';
     this.gameId = id;
-  }
+    this.player1Spots = [];
+    this.player2Spots = [];
 
-var winningCombos = [['a','b','c'],['d','e','f'],['g','h','i'],['a','d','g'],
-['b','e','h'],['c','f','i'],['c','e','g'],['a','e','i']];
-var player1Spots = [];
-var player2spots = [];
-
-
-  startGame() {
-  player1Spots = [];
-  player2Spots = [];
-  var game = new Game(date.Now());
   }
 
   makeMove(spot) {
-    if (this.turn === player1) {
-    player1Spots = player1Spots.concat(spot);
-    this.turn = player2;
+    if (this.turn === 'player1') {
+    this.player1Spots.push(spot);
+    console.log(this.player1Spots);
+    this.turn = 'player2';
   } else {
-    player2Spots = player2Spots.concat(spot);
-    this.turn = player1;
+    this.player2Spots.push(spot);
+    this.turn = 'player1';
   }
 
-    checkForWinner();
-    checkForTie();
+    this.checkForWinner();
+    this.checkForTie();
   }
 
-  checkForWinner(player) {
+
+  checkForWinner() {
+  var winningCombos = [['a','b','c'],['d','e','f'],['g','h','i'],['a','d','g'],
+    ['b','e','h'],['c','f','i'],['c','e','g'],['a','e','i']];
    for (var i = 0; i < winningCombos.length; i++) {
-      if ((player1Spots.includes(win[i][0])) &&
-      (player1Spots.includes(win[i][1])) && (player1Spots.includes(win[i][2]))) {
-        player1.wins.push(game);
-        return 'Player 1 wins!';
-      } else if ((player2Spots.includes(win[i][0])) &&
-      (player2Spots.includes(win[i][1])) && (player2Spots.includes(win[i][2]))) {
-        player2.wins.push(game);
-        return 'Player 2 wins!';)
+      if ((this.player1Spots.includes(winningCombos[i][0])) &&
+      (this.player1Spots.includes(winningCombos[i][1])) && (this.player1Spots.includes(winningCombos[i][2]))) {
+        this.player1.wins.push(this.gameId);
+        console.log('Player 1 wins!');
+      } else if ((this.player2Spots.includes(winningCombos[i][0])) &&
+      (this.player2Spots.includes(winningCombos[i][1])) && (this.player2Spots.includes(winningCombos[i][2]))) {
+        this.player2.wins.push(this.gameId);
+        return 'Player 2 wins!';
       }
     }
   }
 
   checkForTie() {
-    if ((player1Spots.length + player2Spots.length) === 9 ) {
-      return 'This game is a tie!';
-      startGame();
+    if ((this.player1Spots.length + this.player2Spots.length) === 9 ) {
+      console.log('This game is a tie!');
+      return true;
+      //startGame();
     }
+  }
+
+
   }
 
 }
