d87d5a1166f2044a72b1d12240c47d65ebc49a88
kendallha <kendallhaw@gmail.com>
2021-03-05 13:08:23 -0800

    Adjust header styling to align image with text

--- src/main.js
+++ src/main.js
@@ -12,19 +12,18 @@ gameBoard.addEventListener('click', takeATurn);
 
 function startGame() {
   game = new Game(Date.now());
-  game.initiateNewGame();
+  game.keepPlayerScores();
   updateScoreCard();
   for (var i = 0; i < gameSquare.length; i++) {
     gameSquare[i].innerHTML = '';
   }
-  gameInfo.innerHTML = `<h1>It's <img id="token" class="turn-token" src="assets/octopus.png">'s Turn</h1>`
-  // switchTokenInHeader();
+  switchTokenInHeader();
 }
 
 function takeATurn() {
   addToken(event);
-  evaluateGameStatus();
   switchTokenInHeader();
+  evaluateGameStatus();
   updateScoreCard();
 }
 
@@ -38,31 +37,39 @@ function addToken(event) {
   game.makeMove(event.target.id);
 }
 
+// function declareWinner(winner) {
+//   gameInfo.innerHTML = `<h1 class="instructions"><img id="token"
+//   class="turn-token" src= winner /> Wins!</h1>`;
+//   setTimeout(startGame, 3000);
+// }
 function evaluateGameStatus() {
   if (game.winner === 'player1') {
-    gameInfo.innerHTML = `<h1 class="instructions"><img id="token"
+    gameInfo.innerHTML = `<h1 class="header"><img id="token"
     class="turn-token" src="assets/octopus.png"/> Wins!</h1>`;
     setTimeout(startGame, 3000);
+    // declareWinner("assets/octopus.png");
   } else if (game.winner === 'player2') {
-      gameInfo.innerHTML = `<h1 class="instructions"><img id="token"
+      gameInfo.innerHTML = `<h1 class="header"><img id="token"
       class="turn-token" src="assets/lobster.png"/> Wins!</h1>`;
       setTimeout(startGame, 3000);
+      // declareWinner("/Users/kendallhaworth/turing/mod1/tic-tac-toe/assets/lobster.png");
   } else if (game.checkForTie()) {
-      gameInfo.innerHTML = `<h1 class="instructions">It's a Tie!</h2>`
+      gameInfo.innerHTML = `<h1 class="header">It's a Draw!</h2>`
       setTimeout(startGame, 3000);
   }
 }
 
 function switchTokenInHeader() {
   if (game.turn === 'player2') {
-    currentTurnToken.src = "./assets/lobster.png";
+    // currentTurnToken.src = "./assets/lobster.png";
+    gameInfo.innerHTML = `<h1 class="header">It's <img id="token" class="turn-token" src="assets/lobster.png">'s Turn</h1>`;
   } else {
-    currentTurnToken.src = "./assets/octopus.png";
+    // currentTurnToken.src = "./assets/octopus.png";
+    gameInfo.innerHTML = `<h1 class="header">It's <img id="token" class="turn-token" src="assets/octopus.png">'s Turn</h1>`;
   }
 }
 
 function updateScoreCard() {
-
   player1Wins.innerText = `${game.player1.wins.length} Wins`;
   player2Wins.innerText = `${game.player2.wins.length} Wins`;
 
